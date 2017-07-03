d53f516b4240e721cc16860057275df0fa37596f
Kyle Buchanan <kylebuch8@gmail.com>
2017-07-03 17:28:03 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,303 @@
+'use strict';
+
+const Hapi = require('hapi');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const config = require('./config');
+const _ = require('lodash');
+const ObjectId = require('mongodb').ObjectID;
+
+var mongoUrl;
+
+if (process.env.MONGODB_URI) {
+    mongoUrl = MONGODB_URI + 'pagespeed';
+} else {
+    mongoUrl = 'mongodb://localhost:27017/pagespeed';
+}
+
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            cors: true
+        }
+    }
+});
+
+var getResults = (db, callback) => {
+    var collection = db.collection('results');
+    collection.find({}, { limit: 30, sort: [['date', 'desc']] }).toArray((err, items) => {
+        assert.equal(err, null);
+
+        if (items !== null) {
+            callback(items.reverse());
+        }
+    });
+};
+
+var getCurrentScore = (db, callback) => {
+    var collection = db.collection('currentScores');
+    collection.find().toArray((err, item) => {
+        if (item !== null) {
+            callback(item);
+        }
+    });
+};
+
+var getSiteScores = (page, db, callback) => {
+    db.collection('results', (err, collection) => {
+        collection.find({ page: page }, { limit: 30, sort: [['date', 'desc']] }).toArray(function (err, items) {
+            assert.equal(err, null);
+
+            if (items !== null) {
+                callback(items.reverse());
+            }
+        });
+    });
+};
+
+//gets two object and returns difference object
+var difference = function(a, b) {
+    var r = {};
+    _.each(a, function(v, k) {
+        // If it's equal, return now
+        if(b && (b[k] === v || _.isEqual(v, b[k]))) return;
+
+        // If it's an array, the whole thing should be replaced
+        if(Array.isArray(v)){
+            return r[k] = v;
+        }
+
+        // Using a temp variable for the object diff recursion means we can omit empty objects
+        var t = _.isObject(v) ? _.difference(v, b[k]) : v;
+
+        if((_.isObject(t) && !_.isEmpty(t)) || !_.isObject(t)) {
+            r[k] = t;
+        }
+      });
+
+    return r;
+};
+
+//gets list of the unique urls
+var getUrlList = (db, callback) => {
+    db.collection('results').aggregate([
+        { $unwind : '$results' },
+        { $project: {
+            'url': '$results.id'
+            }
+        }
+    ])
+    .toArray(function(err, result) {
+        var finalResult =_.uniqBy(result, 'url');
+        assert.equal(err, null);
+        callback(finalResult);
+    });
+};
+
+//gets dates where score has been changed
+var getMilestoneData = (urlId, strategy, db, callback) => {
+
+
+    var url;
+
+    for (let c = 0, max=config.pages.length; c < max; c++) {
+        if (config.pages[c]['id'] === urlId) {
+            url = config.pages[c]['url'];
+            break;
+        }
+    }
+
+    db.collection('results').aggregate([
+        { $unwind : '$results' },
+        { $match : { 'results.id': url, 'results.strategy': strategy}   },
+        { $sort : { 'date' : -1 } },
+        { $project: {
+                        'date': 1,
+                        'score': '$results.ruleGroups.SPEED.score',
+                        'results': '$results'
+                    }
+        }
+    ])
+    .toArray(function(err, result) {
+        for (let i = 1, max=result.length; i < max; i++) {
+            result[i-1].results.rulePoints = _.map(_.keys(result[i-1].results.formattedResults.ruleResults), function(rule) {
+                var value1,
+                    value2,
+                    diff;
+
+                value1 = result[i-1].results.formattedResults.ruleResults[rule].ruleImpact;
+                value2 = result[i].results.formattedResults.ruleResults[rule].ruleImpact;
+                diff = parseFloat(value1) - parseFloat(value2);
+
+                return {
+                    key: rule,
+                    value: value1,
+                    ruleDiff: diff
+                };
+            });
+
+            result[i-1].results.rulePoints = result[i-1].results.rulePoints.filter(function(item) {
+                return item.value !== 0;
+            });
+        }
+
+        for (let i = 1, max=result.length; i < max; i++) {
+
+            if (result[i].score === result[i-1].score) {
+                result[i-1].state = 0;
+                result[i].state = 1;
+
+            } else {
+                result[i-1].state = 1;
+                result[i].state = 1;
+            }
+
+            result[i-1].ruleDiff = {};
+            _.forIn(_.keys(result[i-1].results.formattedResults.ruleResults), function(rule) {
+
+                var value1 =  result[i-1].results.formattedResults.ruleResults[rule].ruleImpact;
+                var value2 =  result[i].results.formattedResults.ruleResults[rule].ruleImpact;
+
+                result[i-1].ruleDiff[rule] = parseFloat(value1) - parseFloat(value1);
+            });
+
+            result[i-1].scoreDiff = result[i-1].score - result[i].score;
+            result[i-1].pageDiff = difference(result[i].results.pageStats, result[i-1].results.pageStats);
+        }
+
+        var finalResult = result.filter(function(item) {
+            return item.state == 1;
+        });
+
+        assert.equal(err, null);
+        callback(finalResult);
+   });
+};
+
+var getPageById = (id, db, callback) => {
+    db.collection('results', (err, collection) => {
+        collection.find({ _id: ObjectId(id) }, { limit: 30 }).toArray(function (err, items) {
+            assert.equal(err, null);
+
+            if (items !== null) {
+                callback(items);
+            }
+        });
+    });
+};
+
+server.connection({
+    host: 'localhost',
+    port: process.env.PORT || 8000
+});
+
+server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, reply) => {
+        MongoClient.connect(mongoUrl, (err, db) => {
+            assert.equal(null, err);
+            getResults(db, (items) => {
+                db.close();
+                return reply(items);
+            });
+        });
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/pages',
+    handler: (request, reply) => {
+        return reply(config.pages);
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/currentscore',
+    handler: (request, reply) => {
+        MongoClient.connect(mongoUrl, (err, db) => {
+            assert.equal(null, err);
+
+            if (err) {
+                return reply(err);
+            }
+
+            getCurrentScore(db, (data) => {
+                db.close();
+                return reply(data);
+            });
+        });
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/scores',
+    handler: (request, reply) => {
+        MongoClient.connect(mongoUrl, (err, db) => {
+            assert.equal(null, err);
+            getSiteScores(request.query.site, db, (items) => {
+                db.close();
+                return reply(items);
+            });
+        });
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/milestones/{strategy}/{urlId}',
+    handler: (request, reply) => {
+        MongoClient.connect(mongoUrl, (err, db) => {
+            assert.equal(null, err);
+            var urlId = request.params.urlId;
+            var strategy = request.params.strategy;
+
+            getMilestoneData(urlId, strategy, db, function (data) {
+                db.close();
+                return reply(data);
+            });
+        });
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/page/{id}',
+    handler: (request, reply) => {
+        MongoClient.connect(mongoUrl, (err, db) => {
+            assert.equal(null, err);
+            var id = encodeURIComponent(request.params.id);
+            getPageById(id, db, (item) => {
+                db.close();
+                return reply(item);
+            });
+        });
+    }
+});
+
+
+//list of the unique urls form db
+server.route({
+    method: 'GET',
+    path: '/dbsiteslist',
+    handler: (request, reply) => {
+        MongoClient.connect(mongoUrl, (err, db) => {
+            assert.equal(null, err);
+            getUrlList(db, (item) => {
+                db.close();
+                return reply(item);
+            });
+        });
+    }
+});
+
+server.start((err) => {
+    if (err) {
+        throw err;
+    }
+
+    console.log('Hapi server running at: ', server.info.uri);
+});
---
+++ pagespeed.js
@@ -0,0 +1,135 @@
+var psi = require('psi');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var q = require('q');
+var config = require('./config');
+var mongoUrl;
+var apiKeys;
+
+if (!process.env.PS_API_KEY) {
+    apiKeys = require('./keys');
+}
+
+if (process.env.OPENSHIFT_MONGODB_DB_URL) {
+    mongoUrl = process.env.OPENSHIFT_MONGODB_DB_URL + 'pagespeed';
+} else {
+    mongoUrl = 'mongodb://localhost:27017/pagespeed';
+}
+
+var insertDocument = function (data, db, callback) {
+    var deferred = q.defer();
+
+    db.collection('results').insertOne(data, function (err, result) {
+        assert.equal(err, null);
+        console.log('Inserted a document into the results collection.');
+        deferred.resolve(result);
+    });
+
+    return deferred.promise;
+};
+
+var updateCurrentScore = function (page, db) {
+    var deferred = q.defer();
+
+    db.collection('currentScores').update({
+        page: page.page
+    }, page, { upsert: true }, function (err, result) {
+        assert.equal(err, null);
+        console.log('Updated current score');
+        deferred.resolve(result);
+    });
+
+    return deferred.promise;
+};
+
+var getPageSpeedResult = function (url, strategy) {
+    console.log('getPageSpeedResult');
+    return psi(url, {
+        key: process.env.PS_API_KEY || apiKeys.ps_api_key,
+        strategy: strategy
+    }).then(function (data) {
+        data.date = new Date();
+        data.strategy = strategy;
+
+        return data;
+    });
+};
+
+var storePageSpeedResults = function (page) {
+    var deferred = q.defer();
+    var getPageSpeedResultFunctions = [];
+
+    page.strategies.forEach(function (strategy) {
+        getPageSpeedResultFunctions.push(getPageSpeedResult(page.url, strategy));
+    });
+
+    q.all(getPageSpeedResultFunctions).then(function (data) {
+        var result = {
+            page: page.url,
+            title: data[0].title,
+            date: data[0].date,
+            results: data
+        };
+
+        MongoClient.connect(mongoUrl, function (err, db) {
+            assert.equal(null, err);
+            insertDocument(result, db).then(function () {
+                db.close();
+                deferred.resolve(result);
+            });
+        });
+    });
+
+    return deferred.promise;
+};
+
+var getPageSpeedResults = function () {
+    var pageResultFunctions = [];
+
+    config.pages.forEach(function (page) {
+        pageResultFunctions.push(storePageSpeedResults(page));
+    });
+
+    return q.all(pageResultFunctions);
+};
+
+var setCurrentScores = function (pages) {
+    var deferred = q.defer();
+    var pagesData = [];
+    var updateCurrentScoreFunctions = [];
+
+    updateCurrentScoreFunctions.push(pages.forEach(function (page) {
+        var pageDeferred = q.defer();
+        var pageData = {
+            page: page.page,
+            title: page.title,
+            date: page.date
+        };
+
+        page.results.forEach(function (result) {
+            pageData[result.strategy] = result.ruleGroups.SPEED.score;
+        });
+
+        MongoClient.connect(mongoUrl, function (err, db) {
+            assert.equal(null, err);
+            updateCurrentScore(pageData, db).then(function () {
+                db.close();
+                pageDeferred.resolve();
+            });
+        });
+    }));
+
+    q.all(updateCurrentScoreFunctions).then(function () {
+        deferred.resolve(pages);
+    });
+
+    return deferred.promise;
+};
+
+getPageSpeedResults()
+  .then(function (pages) {
+    return setCurrentScores(pages);
+  })
+  .then(function () {
+    console.log('all done!');
+  });
