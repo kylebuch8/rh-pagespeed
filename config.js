45631c577e72d2fe7aa28045b6f2fc8066f3e320
Kyle Buchanan <kylebuch8@gmail.com>
2017-07-03 17:28:56 -0400

    adding the config.js file

---
+++ config.js
@@ -0,0 +1,36 @@
+var config = {
+    pages: [
+        {
+            id: '1',
+            url: 'https://access.redhat.com/',
+            strategies: ['desktop', 'mobile']
+        },
+        {
+            id: '2',
+            url: 'https://access.redhat.com/search/#/',
+            strategies: ['desktop', 'mobile']
+        },
+        {
+            id: '3',
+            url: 'https://access.redhat.com/solutions/100003',
+            strategies: ['desktop', 'mobile']
+        },
+        {
+            id: '4',
+            url: 'https://access.redhat.com/articles/10009',
+            strategies: ['desktop', 'mobile']
+        },
+        {
+            id: '5',
+            url: 'http://www.redhat.com/en',
+            strategies: ['desktop', 'mobile']
+        },
+        {
+            id: '6',
+            url: 'http://developers.redhat.com/',
+            strategies: ['desktop', 'mobile']
+        }
+    ]
+};
+
+module.exports = config;
